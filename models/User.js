a8caf8378d2beecd96b4b3283818916fc18058d9
Amal Shavinda <amalshavinda@gmail.com>
2024-07-24 10:47:10 +0530

    create authentications

---
+++ models/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+export default mongoose.model("user", UserSchema);
